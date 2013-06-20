032f84d12985b311307f800ee33d6f3aba587a0a
ydj <yudejian@163.com>
2013-06-20 11:20:54 +0800

    Initial Commit

---
+++ TestAFont/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TestAFont
+//
+//  Created by YDJ on 13-6-20.
+//  Copyright (c) 2013年 jingyoutimes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
